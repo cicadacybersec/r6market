28f84f071506887afa5fea5c19cb20a500d9ebda
CicadaCyberSec <mac@cicadacybersec.com>
2026-04-19 03:54:22 +0100

    remove backend files for static deploy

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname)));
-
-app.listen(3001, () => {
-  console.log('SiegeVault dev server running at http://localhost:3001');
-});
